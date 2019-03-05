dcc36c66b8bcb99022a5d2bdd254f59054936683
Logan Tawhiri <30024380@student.toiohomai.ac.nz>
2019-03-06 11:22:32 +1300

    gg

---
+++ Task02/Task02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int my = 4;
+
+            Console.WriteLine("Guess My Number Between 1 - 10");
+            int user = int.Parse(Console.ReadLine());
+
+            if (user == my)
+            {
+                Console.WriteLine("Congratz you have guessed the number");
+                Console.ReadLine();
+            }
+            if (user > my)
+            {
+                Console.WriteLine("Your guess is too high");
+                Console.ReadLine();
+            }
+            if (user < my)
+            {
+                Console.WriteLine("Your guess is too low");
+                Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Week3/Week3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Please Enter A Number");
+            int t1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Another Number");
+            int t2 = int.Parse(Console.ReadLine());
+
+            if (t1 > t2)
+            {
+               Console.WriteLine(t1 + " " + "is Greater");
+               Console.ReadLine();
+            }
+            if (t2 > t1)
+            {
+                Console.WriteLine(t2 + " " + "is Greater");
+                Console.ReadLine();
+            }
+            if (t1 == t2)
+            {
+                Console.WriteLine(t1 + " and " + t2 + " " +  "are the same");
+                Console.ReadLine();
+            }     
+            
+
+        }
+    }
+}
