870fd24c1c4cf5fa5bb4e4858981d308812e4cdd
Logan Tawhiri <30024380@student.toiohomai.ac.nz>
2019-03-07 08:55:58 +1300

    IDK

---
+++ Task04/Task04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Number Here");
+            int num = int.Parse(Console.ReadLine());
+            
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("EVEN");
+                Console.ReadLine();               
+            }
+            else
+            {
+                Console.WriteLine("ODD");
+                Console.ReadLine();
+            }
+        }
+    }
+}
