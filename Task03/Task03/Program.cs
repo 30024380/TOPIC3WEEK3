497f12982da7ed977811c1f0c01a24d3ec1356f5
Logan Tawhiri <30024380@student.toiohomai.ac.nz>
2019-03-07 08:40:16 +1300

    donezo

--- Task03/Task03/Program.cs
+++ Task03/Task03/Program.cs
@@ -10,10 +10,49 @@ namespace Task03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter Your Grades");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Please Enter Your Grade 1 - 100");
             int grade = int.Parse(Console.ReadLine());
 
-            if (grade)
-        }
+            if (grade >= 90 && grade <=100)
+            {
+                Console.WriteLine("Grade = A+");
+                Console.ReadLine();
+            }
+            else if(grade >= 80 && grade <=89)
+            {
+                Console.WriteLine("Grade = A");
+                Console.ReadLine();
+            }
+            else if(grade >= 70 && grade <=79)
+            {
+                Console.WriteLine("Grade = B+");
+                Console.ReadLine();
+            }
+            else if(grade >= 60 && grade <=69)
+            {
+                Console.WriteLine("Grade = B");
+                Console.ReadLine();
+            }
+            else if(grade >= 50 && grade <=59)
+            {
+                Console.WriteLine("Grade = C+");
+                Console.ReadLine();
+            }
+            else if(grade >= 40 && grade <=49)
+            {
+                Console.WriteLine("Grade = D");
+                Console.ReadLine();
+            }
+            else if(grade >= 0 && grade <=39)
+            {
+                Console.WriteLine("Grade = F");
+                Console.ReadLine();
+            }
+
+
+
+        }    
     }
 }
