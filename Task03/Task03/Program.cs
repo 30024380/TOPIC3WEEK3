30ea09a7f11143a7798975fddddc3401f19e473b
Logan Tawhiri <30024380@student.toiohomai.ac.nz>
2019-03-07 08:18:23 +1300

    coms

---
+++ Task03/Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Grades");
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade)
+        }
+    }
+}
