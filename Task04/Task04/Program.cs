8d2e06341d838da1b2ec6c57108d0ff8cff3da96
Logan Tawhiri <30024380@student.toiohomai.ac.nz>
2019-03-13 11:12:24 +1300

    comy

--- Task04/Task04/Program.cs
+++ Task04/Task04/Program.cs
@@ -10,19 +10,35 @@ namespace Task04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter A Number Here");
-            int num = int.Parse(Console.ReadLine());
-            
-            if (num % 2 == 0)
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string tryAgain = "y";
+            do
             {
-                Console.WriteLine("EVEN");
-                Console.ReadLine();               
-            }
-            else
-            {
-                Console.WriteLine("ODD");
-                Console.ReadLine();
-            }
+                Console.WriteLine("Enter A Number Here");
+                int num = int.Parse(Console.ReadLine());
+
+                int result;
+                if (int.TryParse(Console.ReadLine(), out result))
+                {
+                    if (num % 2 == 0)
+                    {
+                        Console.WriteLine("EVEN");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("ODD");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                    Console.WriteLine("Invalid Input Try Again");
+                    Console.ReadLine();
+                    Console.WriteLine("Would You Like To Try Again?");
+                    tryAgain = Console.ReadLine();
+            } while (tryAgain == "y");
+           
         }
     }
 }
